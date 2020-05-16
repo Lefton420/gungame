373221b719da83545b5d8eb46c25fde3f3589f4c
Lefton420 <evangowe@gmail.com>
2020-05-16 12:43:59 -0400

    added team setup and more setup stuff to init

---
+++ gamemode/core/sv_teamsetup.lua
@@ -0,0 +1,16 @@
+local plymeta = FindMetaTable("Player")
+
+team.SetUp(0, "Red", Color(255, 0, 0), false)
+team.SetUp(1, "Blue", Color(255, 0, 0), false)
+
+function plymeta:SetupTeam(teamid)
+	self:SetTeam(teamid)
+
+	local col = team.GetColor(teamid)
+	self:SetPlayerColor(Vector(col.r / 255, col.g / 255, col.b / 255)) -- coverts to vector color
+	self:SetHealth(100)
+	self:SetRunSpeed(400)
+	self:SetWalkSpeed(100)
+	self:SetModel("models/player/Group03m/Male_0".. math.random(1,9)..".mdl")
+end
+
--- gamemode/init.lua
+++ gamemode/init.lua
@@ -2,5 +2,47 @@ AddCSLuaFile("cl_init.lua")
 AddCSLuaFile("shared.lua")
 include("shared.lua")
 
+-- Asignes team on player respawn
+function GM:PlayerSpawn(ply)	
+	ply:SetupTeam(math.random(0,1))
+end
 
-print("heloo gamers")
+-- Disables player team freindley fire 
+function GM:PlayerShouldTakeDamage(ply,attacker)
+	if not IsValid(attacker) or not attacker:IsPlayer() then
+		return true
+	end
+	
+	if ply:Team() == attacker:Team() then 
+		return false
+	end
+
+	return true
+end
+
+-- Limits Player text chat to a specified range
+function GM:PlayerCanSeePlayersChat(text,teamonly,listener,speaker)
+	local distance = listener:GetPos():Distance(speaker:GetPos())
+
+	if(distance <= 500) then
+		return true 
+	end
+end
+
+-- Limits player voice chat to a specified range
+function GM:PlayerCanHearPlayersVoice(listener,speaker)
+	return (listener:GetPos():Distance(speaker:GetPos()) < 500)
+end
+
+-- Disables player team freindley fire 
+function GM:PlayerShouldTakeDamage(ply,attacker)
+	if not IsValid(attacker) or not attacker:IsPlayer() then
+		return true
+	end
+	
+	if ply:Team() == attacker:Team() then 
+		return false
+	end
+
+	return true
+end
