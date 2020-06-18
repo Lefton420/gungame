58ecc413ed59efe3d808f2c62f8ea1f6dece16fb
Lefton <unconfigured@null.spigotmc.org>
2020-06-17 23:06:49 -0400

    added scroll bar to tab menu when to many players join

--- gamemode/core/cl_scoreboard.lua
+++ gamemode/core/cl_scoreboard.lua
@@ -13,7 +13,7 @@ local function ToggleScoreboard(toggle)
     if toggle then
         local scrw, scrh = ScrW(), ScrH()
         GGScoreboard = vgui.Create("DFrame")
-        -- Controls the size
+        -- Controls the size of the main panel
         GGScoreboard:SetSize(scrw * .3, scrh * .6)
         GGScoreboard:Center()
         GGScoreboard:SetTitle("")
@@ -25,18 +25,24 @@ local function ToggleScoreboard(toggle)
             surface.DrawRect(0,0,w,h)
             draw.SimpleText("Gun Game", "GG_sb_20", w/2, h*.02, color_white, TEXT_ALIGN_CENTER, TEXT_ALIGN_CENTER)
         end
-        -- controls the player sections
-        local ypos = GGScoreboard:GetTall() * .04
+        
+        local scroll, ypos = vgui.Create("DScrollPanel",GGScoreboard), 0
+        -- This sets offset from where the tabs start ↓
+        scroll:SetPos(0, GGScoreboard:GetTall() * .05)
+        scroll:SetSize(GGScoreboard:GetWide(),GGScoreboard:GetTall() * .97)
+
+        -- Loops through all players and gives them a tab in the menu
         for k,v in pairs(player.GetAll()) do
-            local playerPanel = vgui.Create("DPanel",GGScoreboard)
+            local playerPanel = vgui.Create("DPanel",scroll)
             playerPanel:SetPos(0,ypos)
+            -- This controls how thicc the tabs are ↓ (higher the number the thiccer they are)
             playerPanel:SetSize(GGScoreboard:GetWide(), GGScoreboard:GetTall() * .05)
             local name,ping = v:Name(), v:Ping()
             playerPanel.Paint = function(self,w,h)
                 if IsValid(v) then
                     surface.SetDrawColor(0,0,0,200)
                     surface.DrawRect(0,0,w,h)
-                    draw.SimpleText(name .. "Ping:".. ping ,"GG_sb_20",w/2,h/2, color_white, TEXT_ALIGN_CENTER, TEXT_ALIGN_CENTER)
+                    draw.SimpleText(name .. " Ping:".. ping ,"GG_sb_20",w/2,h/2, color_white, TEXT_ALIGN_CENTER, TEXT_ALIGN_CENTER)
                 end
             end
             ypos = ypos + playerPanel:GetTall() * 1.1
