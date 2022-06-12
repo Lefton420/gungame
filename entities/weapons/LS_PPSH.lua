fc8b6e8e239ac43fd133bd1f6a9a9e4720c5ba85
Lefton <unconfigured@null.spigotmc.org>
2022-06-11 21:59:50 -0400

    Update LS_PPSH.lua

--- entities/weapons/LS_PPSH.lua
+++ entities/weapons/LS_PPSH.lua
@@ -26,8 +26,8 @@ SWEP.Primary.Sound = Sound("Weapon_USP.SilencedShot")
 SWEP.Primary.Recoil = .1 -- base recoil value, SWEP.Spread mods can change this
 SWEP.Primary.Damage = 8
 SWEP.Primary.NumShots = 1
-SWEP.Primary.Cone = 0.04
-SWEP.Primary.Delay = RPM(450)
+SWEP.Primary.Cone = 0.03
+SWEP.Primary.Delay = RPM(999)
 
 SWEP.Primary.Burst = false
 
@@ -37,10 +37,10 @@ SWEP.Primary.ClipSize = 15
 SWEP.Primary.DefaultClip = 30
 
 SWEP.Spread = {}
-SWEP.Spread.Min = .1
-SWEP.Spread.Max = 0.8
+SWEP.Spread.Min = .01
+SWEP.Spread.Max = 0.3
 SWEP.Spread.IronsightsMod = 1.5 -- multiply
-SWEP.Spread.CrouchMod = 0.9 -- crouch effect (multiply)
+SWEP.Spread.CrouchMod = 1.0 -- crouch effect (multiply)
 SWEP.Spread.AirMod = 2 -- how does if the player is in the air effect spread (multiply)
 SWEP.Spread.RecoilMod = 0.3 -- how does the recoil effect the spread (sustained fire) (additional)
 SWEP.Spread.VelocityMod = 1.3 -- movement speed effect on spread (additonal)
@@ -50,7 +50,7 @@ SWEP.IronsightsAng = Angle(0.2, -0.201, 0)
 SWEP.IronsightsFOV = 0.72
 SWEP.IronsightsSensitivity = 0.8
 SWEP.IronsightsCrosshair = false
-SWEP.IronsightsRecoilVisualMultiplier = 0.5
+SWEP.IronsightsRecoilVisualMultiplier = 0.8
 
 SWEP.LoweredPos = Vector(0, -16, -13)
 SWEP.LoweredAng = Angle(70, 0, 0)
